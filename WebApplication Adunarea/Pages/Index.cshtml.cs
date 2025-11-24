6e734b94a53fd89e90ac21adc343f793da5bbf68
user <user@WIN-4RL1QGQ23MT>
2025-11-24 14:10:43 +0200

    Add project files.

---
+++ WebApplication Adunarea/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_Adunarea.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // Nu avem nevoie de logic? C# pentru acest exerci?iu
+        public void OnGet()
+        {
+            // Pagina se incarca
+        }
+    }
+}
